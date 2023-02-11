89ed8da91564b843bd952cbfd83e1c9375695609
Perryjay30 <pelumijsh@gmail.com>
2023-02-10 12:52:21 -1200

    VerifyOtpRequest

---
+++ src/main/java/com/starlightonlinestore/data/dto/Request/VerifyOtpRequest.java
@@ -0,0 +1,12 @@
+package com.starlightonlinestore.data.dto.Request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class VerifyOtpRequest {
+    @NotBlank(message = "This field must not be empty")
+    private String token;
+    @NotBlank(message = "This field must not be empty")
+    private String email;
+}
