45ce8a26d160e707c79ee8c872266ec9f229eb8d
Perryjay30 <pelumijsh@gmail.com>
2023-02-10 12:42:26 -1200

    ChangePasswordRequest

---
+++ src/main/java/com/starlightonlinestore/data/dto/Request/ChangePasswordRequest.java
@@ -0,0 +1,25 @@
+package com.starlightonlinestore.data.dto.Request;
+
+import com.starlightonlinestore.utils.validators.UserDetailsValidator;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import org.mindrot.jbcrypt.BCrypt;
+
+@Data
+public class ChangePasswordRequest {
+    @NotBlank(message = "This field must not be empty")
+    private String email;
+    @NotBlank(message = "This field must not be empty")
+    private String oldPassword;
+    @NotBlank(message = "This field must not be empty")
+    private String newPassword;
+
+
+    public String getNewPassword() {
+        if(UserDetailsValidator.isValidPassword(newPassword))
+            return BCrypt.hashpw(newPassword, BCrypt.gensalt());
+        else
+            throw new RuntimeException("password must contain at least one " +
+                    "capital letter, small letter and special characters");
+    }
+}
