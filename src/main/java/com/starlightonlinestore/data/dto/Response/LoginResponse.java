fa67167ac9e3cd5b23cecb2ae33db9cabd1003a4
Perryjay30 <pelumijsh@gmail.com>
2023-12-06 16:14:54 +0100

    Refactoring

--- src/main/java/com/starlightonlinestore/data/dto/Response/LoginResponse.java
+++ src/main/java/com/starlightonlinestore/data/dto/Response/LoginResponse.java
@@ -1,8 +1,11 @@
 package com.starlightonlinestore.data.dto.Response;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class LoginResponse {
     private String message;
+    private String loginToken;
 }
